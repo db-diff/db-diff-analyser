fd7cf5200b34ebb17c9fd5db32c6294179c89172
Cédric Maria-Sube <ext-mariasube@em-lyon.com>
2022-10-18 14:59:06 +0200

    Adds the structure for a Spring Boot application.

---
+++ src/main/java/io/dbdiff/dbdiffanalyser/DbDiffAnalyserApplication.java
@@ -0,0 +1,13 @@
+package io.dbdiff.dbdiffanalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbDiffAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbDiffAnalyserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/dbdiff/dbdiffanalyser/DbDiffAnalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dbdiff.dbdiffanalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbDiffAnalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
