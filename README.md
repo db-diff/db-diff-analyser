803624927856d8a13475bd90665c66fd956e3fea
Cédric Maria-Sube <ext-mariasube@em-lyon.com>
2022-10-18 16:08:39 +0200

    Adds a CLI.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # db-diff-analyser
 A tool to compare two databases during migrations.
+
+## Build
+
+`gradlew build`
+
+## Run
+
+`java -jar ./build/libs/dbdiffanalyser-0.0.1-SNAPSHOT.jar`
--- src/main/java/io/dbdiff/dbdiffanalyser/DbDiffAnalyserApplication.java
+++ src/main/java/io/dbdiff/dbdiffanalyser/DbDiffAnalyserApplication.java
@@ -1,13 +1,29 @@
 package io.dbdiff.dbdiffanalyser;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.java.Log;
+
 @SpringBootApplication
-public class DbDiffAnalyserApplication {
+@Log
+public class DbDiffAnalyserApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+		log.info("Starting the application.");
+
 		SpringApplication.run(DbDiffAnalyserApplication.class, args);
+
+		log.info("Application finished.");
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("EXECUTING : command line runner");
+
+		for (int i = 0; i < args.length; ++i) {
+			log.info("args[" + i + "]: " + args[i]);
+		}
+	}
 }
